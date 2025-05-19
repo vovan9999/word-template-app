694e5c83ae94dffc5e41d64a068d4e6c0b2f245a
Volodymyr Yanchuk <volodymyr.yanchuk@uniqa.ua>
2025-05-20 02:44:48 +0300

    Final: gunicorn working1

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def generate():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host='0.0.0.0', port=8080)
+    app.run(debug=False, host='0.0.0.0', port=port)
